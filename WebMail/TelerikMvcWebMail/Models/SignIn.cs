a24627d1561fa8801f96cbd0da52e4302f4e1813
Weiying Chen <v-weiyc@microsoft.com>
2017-10-05 12:24:19 -0700

    '9-3'

---
+++ WebMail/TelerikMvcWebMail/Models/SignIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcWebMail.Models
+{
+    public class SignIn
+    {
+        [Required(ErrorMessage = "User Name Required")]
+        public string UserName { get; set; }
+        
+    }
+
+    public partial class UserViewModel
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
